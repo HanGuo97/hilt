1a1826c47c69856838ad537e01756826ccf7597e
Han Guo <alex.guohan1106@gmail.com>
2025-07-27 22:17:33 -0700

    added `debug_utils`

---
+++ cutedsl_utilities/debug_utils.py
@@ -0,0 +1,25 @@
+# https://github.com/NVIDIA/cutlass/blob/main/include/cute/util/debug.hpp
+
+import cutlass.cute as cute
+
+
+def block(bid: int) -> bool:
+    bidx, bidy, bidz = cute.arch.block_idx()
+    gdimx, gdimy, _ = cute.arch.grid_dim()
+    current_block_id = bidx + bidy * gdimx + bidz * gdimx * gdimy
+    return current_block_id == bid
+
+
+def thread(tid: int, bid: int) -> bool:
+    tidx, tidy, tidz = cute.arch.thread_idx()
+    bdimx, bdimy, _ = cute.arch.block_dim()
+    current_thread_id = tidx + tidy * bdimx + tidz * bdimx * bdimy
+    return (current_thread_id == tid) and block(bid)
+
+
+def thread0() -> bool:
+    return thread(0, 0)
+
+
+def block0() -> bool:
+    return block(0)
