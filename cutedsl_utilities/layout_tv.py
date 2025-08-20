2a1e8d43c83a6eb1eecbd5a7f9e8cb286859e7ef
Han Guo <alex.guohan1106@gmail.com>
2025-08-20 14:50:19 -0700

    layout_tv: simpler

--- cutedsl_utilities/layout_tv.py
+++ cutedsl_utilities/layout_tv.py
@@ -1,10 +1,10 @@
-import os
-import torch
-import tempfile
-import subprocess
-from jinja2 import Environment
-from torch.utils.cpp_extension import load_inline
-from cutedsl_utilities.pycute_utils import idx2crd, crd2idx
+import matplotlib.pyplot as plt
+from cutedsl_utilities.pycute_utils import (
+    Layout,
+    idx2crd,
+    crd2idx,
+    visualize_layout,
+)
 
 TilerMN = tuple[int, int]
 TVShape = tuple[tuple[int, int] | int, tuple[int, int] | int]
@@ -18,139 +18,23 @@ __all__ = [
 
 def visualize_layout_tv(
     tiler_mn: TilerMN,
-    layout_tv: LayoutTV,
-    dtype: torch.dtype,
-    copy_bytes: int | None = None,
-    template_path: str | None = None,
-    cutlass_path: str | None = None,
-    inline: bool = False,
-) -> tuple[str, str]:
-    """
-    Visualize layout using CuTe tiled copy and generate LaTeX output.
+    layout_tv: Layout,
+    **kwargs,
+) -> tuple[plt.Figure, plt.Axes]:
 
-    Args:
-        tiler_mn: Tiler dimensions (m, n)
-        layout_tv: Thread-value layout specification
-        dtype: PyTorch dtype
-        copy_bytes: Copy operation size in bytes (e.g., 16)
-        template_path: Optional path to the template file
-        cutlass_base_dir: Optional CUTLASS base directory
-
-    Returns:
-        LaTeX representation of the Layout TV and rendered CPP source.
-    """
-
-    if dtype == torch.float16:
-        element_type = "cute::half_t"
-        element_bytes = 16
-    elif dtype == torch.bfloat16:
-        element_type = "cute::bfloat16_t"
-        element_bytes = 16
-    elif dtype == torch.float32:
-        element_type = "float"
-        element_bytes = 32
-    else:
-        raise NotImplementedError
-
-    if template_path is None:
-        template_path = os.path.join(
-            os.path.dirname(__file__),
-            "csrc/utils/tv_layout_tool.cpp",
+    def label_map(index: int) -> str:
+        thr_crd, val_crd = idx2crd(
+            idx=index,
+            shape=layout_tv.shape,
+            stride=layout_tv.stride,
         )
+        return f"T: {thr_crd}\nV: {val_crd}"
 
-    if cutlass_path is None:
-        cutlass_path = os.path.join(
-            os.path.dirname(__file__),
-            "../cutlass/",
-        )
-
-    tiler_m, tiler_n = tiler_mn
-    (
-        (
-            (thr_shape_m, thr_shape_n),
-            (val_shape_m, val_shape_n),
-        ),
-        (
-            (thr_stride_m, thr_stride_n),
-            (val_stride_m, val_stride_n),
-        ),
-    ) = layout_tv
-
-    with open(template_path) as f:
-        template_content = f.read()
-    environment = Environment()
-    template = environment.from_string(template_content)
-    cpp_source = template.render(
-        element_type=element_type,
-        tiler_m=tiler_m,
-        tiler_n=tiler_n,
-        thr_shape_m=thr_shape_m,
-        thr_shape_n=thr_shape_n,
-        val_shape_m=val_shape_m,
-        val_shape_n=val_shape_n,
-        thr_stride_m=thr_stride_m,
-        thr_stride_n=thr_stride_n,
-        val_stride_m=val_stride_m,
-        val_stride_n=val_stride_n,
-        copy_bytes=copy_bytes if copy_bytes is not None else element_bytes,
+    tiler_layout = Layout(tiler_mn)
+    return visualize_layout(
+        layout=tiler_layout,
+        label_map=label_map,
     )
-
-    include_paths = [
-        os.path.join(cutlass_path, "include"),
-        os.path.join(cutlass_path, "tools/util/include"),
-    ]
-
-    if inline:
-        # JIT compile using PyTorch
-        module = load_inline(
-            name="tv_layout_tool",
-            cpp_sources=cpp_source,
-            functions=["visualize_layout_tv"],
-            extra_include_paths=include_paths,
-            with_cuda=True,
-        )
-        output = module.visualize_layout_tv()
-    else:
-        output = compile_and_run(
-            code=cpp_source,
-            include_paths=include_paths,
-        )
-    return output, cpp_source
-
-
-def compile_and_run(code: str, include_paths: list[str]) -> str:
-    with tempfile.TemporaryDirectory() as temp_dir:
-        # Save source code to temporary file
-        source_file = os.path.join(temp_dir, "temp.cpp")
-        executable_file = os.path.join(temp_dir, "temp")
-
-        with open(source_file, "w") as f:
-            f.write(code)
-
-        # Build nvcc compile command
-        compile_cmd = ["nvcc", "-std=c++17", source_file, "-o", executable_file]
-        for include_path in include_paths:
-            compile_cmd.extend(["-I", include_path])
-        try:
-            subprocess.run(
-                compile_cmd,
-                check=True,
-                capture_output=True,
-                text=True,
-            )
-        except subprocess.CalledProcessError as e:
-            raise RuntimeError(f"Compilation failed: {e.stderr}")
-
-        try:
-            output = subprocess.run(
-                [executable_file],
-                check=True,
-                capture_output=True,
-                text=True,
-            )
-            return output.stdout
-        except subprocess.CalledProcessError as e:
-            raise RuntimeError(f"Execution failed: {e.stderr}")
 
 
 def tiler_crd_to_layout_tv_crd(
