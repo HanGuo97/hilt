f5ecf748f3309bb7a5ebd05094fa2e8568f63b2a
Han Guo <alex.guohan1106@gmail.com>
2025-08-23 15:16:14 -0700

    layout_tv: minor changes

--- cutedsl_utilities/layout_tv.py
+++ cutedsl_utilities/layout_tv.py
@@ -3,7 +3,9 @@ from cutedsl_utilities.pycute_utils import (
     Layout,
     idx2crd,
     crd2idx,
+    product,
     visualize_layout,
+    default_color_map,
 )
 
 TilerMN = tuple[int, int]
@@ -19,20 +21,46 @@ __all__ = [
 def visualize_layout_tv(
     tiler_mn: TilerMN,
     layout_tv: Layout,
+    use_index: bool = False,
     **kwargs,
 ) -> tuple[plt.Figure, plt.Axes]:
 
-    def label_map(index: int) -> str:
+    assert len(layout_tv.shape) == 2
+    assert len(layout_tv.stride) == 2
+
+    def get_crd_and_idx(index: int) -> tuple[tuple[int, ...], tuple[int, ...], int, int]:
         thr_crd, val_crd = idx2crd(
             idx=index,
             shape=layout_tv.shape,
             stride=layout_tv.stride,
         )
-        return f"T: {thr_crd}\nV: {val_crd}"
+        thr_idx = crd2idx(
+            crd=thr_crd,
+            shape=layout_tv.shape[0],
+        )
+        val_idx = crd2idx(
+            crd=val_crd,
+            shape=layout_tv.shape[1],
+        )
+        return thr_crd, val_crd, thr_idx, val_idx
+
+    def color_map(index: int) -> tuple[float, float, float, float]:
+        thr_crd, val_crd, thr_idx, val_idx = get_crd_and_idx(index)
+        rgb = default_color_map(thr_idx)
+        alpha = (val_idx + 1) / product(layout_tv.shape[1])
+        return *rgb, alpha
+
+    def label_map(index: int) -> str:
+        thr_crd, val_crd, thr_idx, val_idx = get_crd_and_idx(index)
+        if not use_index:
+            return f"T: {thr_crd}\nV: {val_crd}"
+        else:
+            return f"T: {thr_idx}\nV: {val_idx}"
 
     tiler_layout = Layout(tiler_mn)
     return visualize_layout(
         layout=tiler_layout,
+        color_map=color_map,
         label_map=label_map,
         **kwargs
     )
