6f07553986f9ef7628a5c9f9bedde263164f7cb3
Han Guo <alex.guohan1106@gmail.com>
2025-08-23 21:36:11 -0700

    minor changes

--- cutedsl_utilities/__init__.py
+++ cutedsl_utilities/__init__.py
@@ -1,6 +1,6 @@
 __version__ = "0.0.1"
 
-from .layout_tv import visualize_layout_tv
-from .debug_utils import *
+from .layout_tv import *
 from .math_utils import *
+from .debug_utils import *
 from .pycute_utils import *
