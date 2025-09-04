9e75fb195fd2449995c4d61bd8c705de0871e7e7
hanguo97 <alex.guohan1106@gmail.com>
2025-09-03 22:57:24 -0400

    added `layout_utils`

---
+++ cutedsl_utilities/layout_utils.py
@@ -0,0 +1,9 @@
+import cutlass
+import cutlass.cute as cute
+
+
+@cute.jit
+def idx2crd(idx: int, shape: tuple[int, ...]) -> tuple[int, ...]:
+    # https://github.com/NVIDIA/cutlass/issues/2473
+    id_layout = cute.make_identity_layout(shape)
+    return id_layout(idx)
